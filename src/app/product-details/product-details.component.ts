8df993c4715fa7ef261935d5eb2b4aac4a2c722b
brightmohan <brightmohan@gmail.com>
2021-11-07 11:24:14 +1100

    Add explicit angular decorator on the ProductDetailsComponent class

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -3,6 +3,11 @@ import { ActivatedRoute } from '@angular/router';
 
 import { Product, products } from '../products';
 
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css'],
+})
 export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
 
